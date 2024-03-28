909a29206298551920b85a12954eb652af1f29a0
gustavo <gustavo767psal@gmail.com>
2024-03-28 16:52:16 -0300

    exemplos

--- avaliativa/atvd2.php
+++ avaliativa/atvd2.php
@@ -2,7 +2,27 @@
 
 class Veiculo{
 
+    private $marca;
+    private $modelo;
+    private $ano;
+    private $cor;
+    private $preco_zero
     
+    public function __construct(string $marca, string $modelo, int $ano, string $cor, double $preco_zero){
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+        $this->cor = $cor;
+        $this->preco_zero = $preco_zero;
+    }
+
+    public function ExibirDadosPessoa(){
+        echo "================================<br>
+        Nome: $this->nome<br>
+        Idade: $this->idade<br>
+        Profissão: $this->profissao<br>
+        ================================<br>";
+    }
 
 }
 

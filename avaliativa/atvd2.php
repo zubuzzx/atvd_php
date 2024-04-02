df6d892197559f95049ad54e72505ff638f533a7
gustavo <gustavo767psal@gmail.com>
2024-04-02 15:17:14 -0300

    finalizado

--- avaliativa/atvd2.php
+++ avaliativa/atvd2.php
@@ -1,30 +1,56 @@
 <?php
-
 class Veiculo{
+ 
+ private $marca;
+ private $modelo;
+ private $ano;
+ private $cor;
+ private $preco_zero;
+ public $idade = 0;
+ public $VD = 0;
 
-    private $marca;
-    private $modelo;
-    private $ano;
-    private $cor;
-    private $preco_zero
-    
-    public function __construct(string $marca, string $modelo, int $ano, string $cor, double $preco_zero){
-        $this->marca = $marca;
-        $this->modelo = $modelo;
-        $this->ano = $ano;
-        $this->cor = $cor;
-        $this->preco_zero = $preco_zero;
-    }
+ public function __construct(string $marca, string $modelo, int $ano, string $cor, int $preco_zero){
+     $this->marca = $marca;
+     $this->modelo = $modelo;
+     $this->ano = $ano;
+     $this->cor = $cor;
+     $this->preco_zero = $preco_zero;
+ }
 
-    public function ExibirDadosPessoa(){
-        echo "================================<br>
-        Marca: $this->marca<br>
-        Modelo: $this->modelo<br>
-        Ano: $this->ano<br>
-        Cor: $this->cor<br>
-        Preço carro zero: $this->preco_zero<br>
-        ================================<br>";
-    }
+ public function ExibirDadosCarro(){
+     echo "================================<br>
+     Marca: $this->marca<br>
+     Modelo: $this->modelo<br>
+     Ano: $this->ano<br>
+     Cor: $this->cor<br>
+     Preço carro zero: $this->preco_zero<br>
+     ================================<br>";
+ }
+
+ public function Calcularidade(){
+    $this->idade = (2024 - $this->ano); 
+    echo "idade do carro: $this->idade anos<br>
+    ================================<br>";
+ }
+
+ public function calcularDepreciacao(){
+    $this->VD = ((2024 - $this->ano) * 430) + 2100;
+    echo "valor de depreciação: $this->VD<br>
+    ================================<br>";
+ }
+
+ public function ehNovo(){
+    if ($this->idade > 2 ) {
+        echo "carro antigo <br>";
+    } else {
+        echo "carro novo <br>";
+    };
+    echo "================================<br>";
+ }
+
+ public function pintarVeiculo($novaCor){
+    $this->cor = $novaCor; 
+ }
 
 }
 
--- avaliativa/poo1.php
+++ avaliativa/poo1.php
@@ -1,14 +1,20 @@
 <?php
 
-require_once('atvd1.php, atvd2.php');
+require_once('atvd1.php');
+require_once('atvd2.php');
 
 $Pessoa1 = new Pessoa("Gustavo", 19, "programador");
 $Pessoa2 = new Pessoa("Pedro", 19, "hacker");
 
-$Veiculo = new Veiculo('FIAT', '')
-
+$Veiculo = new Veiculo('Fiat', 'toro', 2011, 'preto', 2000);
+//atividade 1 pessoa
 $Pessoa1->ExibirDadosPessoa();
 $Pessoa2->ExibirDadosPessoa();
-
-
+//atividade 2 carro
+$Veiculo->ExibirDadosCarro();
+$Veiculo->Calcularidade();
+$Veiculo->calcularDepreciacao();
+$Veiculo->ehNovo();
+$Veiculo->pintarVeiculo("vermelho");
+$Veiculo->ExibirDadosCarro();
 ?>

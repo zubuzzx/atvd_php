6d4051d661f1f10b792eed81985fba942af4df81
gustavo <gustavo767psal@gmail.com>
2024-04-09 15:08:09 -0300

    inserção. edição, listagem e remoção de BDD

--- atvd-avaliativa/class/Produtos_class.php
+++ atvd-avaliativa/class/Produtos_class.php
@@ -15,23 +15,50 @@ class Produtos
     public $id_fornecedor;
 
 
-    // Métodos:
     public function Cadastrar()
     {
         $sql = "INSERT INTO produtos(nome, preco, quantidade, id_categoria, id_fornecedor) VALUES (?,?,?,?,?)";
         $conexao = Banco::conectar();
-        // Converter o comando sql (string) em um objeto:
         $comando = $conexao->prepare($sql);
-        //executar o comando:
-        //essa parte esta dando erro por ter uma chave estrangeira
         $comando->execute([$this->nome, $this->preco, $this->quantidade, $this->id_categoria, $this->id_fornecedor]);
         $linhas = $comando->rowCount();
+
         Banco::desconectar();
-        // Retornar a qtd de linhas cadastradas:
         return $linhas;
     }
+
+    public function Editar(){
+        $sql = "UPDATE produtos SET nome=?, preco=?, quantidade=?, id_categoria=?, id_fornecedor=? WHERE id=?";
+        $conexao = Banco::conectar();
+        $comando = $conexao->prepare($sql);
+        $comando->execute([$this->nome, $this->preco, $this->quantidade, $this->id_categoria, $this->id_fornecedor]);
+        $linhas = $comando->rowCount();
+
+        Banco::desconectar();
+        return $linhas;
+    }
+
+    public function Listar()
+    {
+        $sql = "SELECT * FROM produtos";
+        $conexao = Banco::conectar();
+        $comando = $conexao->prepare($sql);
+        $comando->execute();
+        $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+        Banco::desconectar();
+        return $resultado;
+    }
+
     public function Apagar(){
-        $sql = "DELETE FROM usuarios WHERE id = ?";
+        $sql = "DELETE FROM produtos WHERE id = ?";
+        $conexao = Banco::conectar();
+        $comando = $conexao->prepare($sql);
+        $comando->execute([$this->id]);
+        $linhas = $comando->rowCount();
+
+        Banco::desconectar();
+        return $linhas;
 
     }
 }
--- atvd-avaliativa/index.php
+++ atvd-avaliativa/index.php
@@ -4,12 +4,28 @@ require_once('class/Produtos_class.php');
 
 $produtos = new Produtos();
 
-$produtos->nome = "Sabão em pó";
-$produtos->preco = 20;
-$produtos->quantidade = 177;
+$produtos->nome = "Presunto";
+$produtos->preco = 17;
+$produtos->quantidade = 52;
 $produtos->id_categoria = 1;
 $produtos->id_fornecedor = 1;
 
-echo $produtos->Cadastrar();
+//para cadastramento de um produto usar o comando abaixo:
+//echo $produtos->Cadastrar();
 
+//agora para edição de um produto troque as informações que deseja editar e use o comando abaixo:
+//echo $produto->Editar();
+
+//comando para listagem dos produtos já cadastrados no banco de dados:
+$tabela = $produtos->Listar();
+foreach($tabela as $linha){
+         echo "nome do produto: ".$linha['nome']."<br>
+         preco do produto: ".$linha['preco']."<br>
+         quantidade em estoque: ".$linha['quantidade']."<br>
+         ====================================================<br>";
+}
+
+//use esse comando para apagar:
+//$produto->id = 1;
+// echo $usuario->Apagar();
 ?>
